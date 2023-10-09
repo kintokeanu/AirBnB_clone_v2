a4d78230cbaafc6c170e3bc19a72d69445e02652
kintokeanu <brianislevu@gmail.com>
2023-10-09 03:07:13 +0300

    prepping servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,54 @@
 #!/usr/bin/env bash
 # sets up both webservers for deployment
+# Function to check if a symbolic link exists and points to a specific target
+symlink_exists() {
+    local symlink=$1
+    local target=$2
+
+    [[ -L $symlink && $(readlink -f $symlink) == $target ]]
+}
+
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
     sudo apt-get install -y nginx
 fi
 
-# Create necessary directories if they don't exist
+# Define web_static directory paths
 web_static_dir="/data/web_static"
-test_release_dir="$web_static_dir/releases/test"
+releases_dir="$web_static_dir/releases"
+shared_dir="$web_static_dir/shared"
+current_dir="$web_static_dir/current"
+test_release_dir="$releases_dir/test"
 
-sudo mkdir -p "$web_static_dir/shared"
-sudo mkdir -p "$test_release_dir"
+# Create necessary directories if they don't exist
+sudo mkdir -p $releases_dir $shared_dir $test_release_dir
 
-# Create a fake HTML file
+# Create a fake HTML file for testing
 fake_html_file="$test_release_dir/index.html"
-echo "Fake HTML content" | sudo tee "$fake_html_file" > /dev/null
-
-# Create or recreate the symbolic link
-current_link="$web_static_dir/current"
-if [ -L "$current_link" ]; then
-    sudo rm -f "$current_link"
-fi
-sudo ln -s "$test_release_dir" "$current_link"
-
-# Give ownership of /data/ to ubuntu user and group (recursively)
-sudo chown -R ubuntu:ubuntu "$web_static_dir"
-
-# Update Nginx configuration
-nginx_config_file="/etc/nginx/sites-available/default"
-nginx_alias_config="location /hbnb_static/ {\n    alias $web_static_dir/current/;\n}\n"
-
-if sudo grep -q "location /hbnb_static/" "$nginx_config_file"; then
-    # Replace the existing alias block
-    sudo sed -i "/location \/hbnb_static\//,/^}/c$nginx_alias_config" "$nginx_config_file"
-else
-    # Add the alias block if it doesn't exist
-    sudo sed -i "/location /i$nginx_alias_config" "$nginx_config_file"
+if [ ! -f $fake_html_file ]; then
+    echo "Hello, this is a test page." | sudo tee $fake_html_file > /dev/null
 fi
 
-# Restart Nginx
-sudo service nginx restart
+# Check if the symbolic link already exists and is correct
+if ! symlink_exists $current_dir $test_release_dir; then
+    # Remove the existing symbolic link if it's not correct or doesn't exist
+    sudo rm -f $current_dir
+    # Create the symbolic link
+    sudo ln -s $test_release_dir $current_dir
+fi
+
+# Give ownership of the /data/ directory to the ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu $web_static_dir
+
+# Check if the Nginx configuration already contains the required alias
+if ! grep -q "location /hbnb_static/ {" /etc/nginx/sites-enabled/default; then
+    # Remove the existing alias block if it exists
+    sudo sed -i '/location \/hbnb_static\//,/\}/d' /etc/nginx/sites-enabled/default
+    # Add the alias block to the Nginx configuration
+    echo "location /hbnb_static/ {" | sudo tee -a /etc/nginx/sites-enabled/default
+    echo "    alias $current_dir/;" | sudo tee -a /etc/nginx/sites-enabled/default
+    echo "}" | sudo tee -a /etc/nginx/sites-enabled/default
+    # Restart Nginx to apply the configuration
+    sudo service nginx restart
+fi
