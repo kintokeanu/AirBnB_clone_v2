15ccc3b47179f4826006f1c3dcc7edc38dbe608b
kintokeanu <brianislevu@gmail.com>
2023-10-22 03:42:20 +0300

    starting flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.get('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Root path request handler.
