aa012b88462e526f27c9e78cb31db8671f5b9669
kintokeanu <brianislevu@gmail.com>
2023-10-22 20:15:49 +0300

    handling state

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage sorted by name (A->Z).
+    """
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    Closes the current SQLAlchemy Session.
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
